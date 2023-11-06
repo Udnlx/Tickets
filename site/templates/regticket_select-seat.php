9d22278c3c593f1d3e375514dc149fe0a013286b
Udnlx <Udnlx@yandex.ru>
2023-11-06 10:32:11 +0300

    перенос распечатки для водителя на страницу отчета по рейсу

--- site/templates/regticket_select-seat.php
+++ site/templates/regticket_select-seat.php
@@ -169,25 +169,6 @@ $passengers .= '
                         <p class="messages" style="color: green;"></p>
                     </div>
                 </form>
-
-                <form class="uk-flex uk-flex-column" id="print_select_seat" action="/otchet-po-reisu-dlia-voditelia/" method="post">
-                    <div class="uk-margin-small-top uk-hidden">
-                        <input class="uk-input" id="print_selected_bus" type="text" name="print_selected_bus" value="<?php echo $selected_bus; ?>">
-                    </div>
-                    <div class="uk-margin-small-top uk-hidden">
-                        <input class="uk-input" id="print_selected_id_bus" type="text" name="print_selected_id_bus" value="<?php echo $selected_id_bus; ?>">
-                    </div>
-                    <div class="uk-margin-small-top uk-hidden">
-                        <input class="uk-input" id="print_selected_date" type="text" name="print_selected_date" value="<?php echo $selected_date; ?>">
-                    </div>
-                    <div class="uk-margin-small-top uk-hidden">
-                        <input class="uk-input" id="print_selected_time" type="text" name="print_selected_time" value="<?php echo $selected_time; ?>">
-                    </div>
-                    
-                    <div class="uk-margin-small-top uk-flex uk-flex-column">
-                        <button class="uk-margin-small-top uk-button uk-button-default" type="submit">Распечатка для водителя</button>
-                    </div>
-                </form>
             </div>
         </div>
         

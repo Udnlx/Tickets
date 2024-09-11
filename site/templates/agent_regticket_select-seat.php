c8c6ed51081e0ae5bad938907031a93378d9bd94
Udnlx <Udnlx@yandex.ru>
2024-09-11 13:30:44 +0300

    для агентов реестр купленных мест скрыли некоторые данные

--- site/templates/agent_regticket_select-seat.php
+++ site/templates/agent_regticket_select-seat.php
@@ -104,7 +104,7 @@ $data_passenger = $pages->get('template=passengers, id=' . $val['id_passenger'] 
 $phone_passenger = $data_passenger->phone_passenger;
 $birthday_passenger = $data_passenger->birthday_passenger;
 $reestr_seat .= '
-    <p class="reestr_seat_item">Место - ' . $val['seat'] . ' - ' . $val['pay_or_booking'] . ' - ' . $val['confirm'] . '<br> Станция посадки: ' . $val['station'] . '<br>' . $val['passenger'] . '<br>' . $birthday_passenger . '<br>тип билета: ' . $val['type_ticket'] . '<br>агент: ' . $val['agent_ticket'] . '<br><span> - Регистратор: ' . $val['operator'] . '</span></p>
+    <p class="reestr_seat_item">Место - ' . $val['seat'] . ' - ' . $val['pay_or_booking'] . ' - ' . $val['confirm'] . '<br> Станция посадки: ' . $val['station'] . '<br>' . $val['passenger'] . '<br>тип билета: ' . $val['type_ticket'] . '</p>
 ';
 }
 

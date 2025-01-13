5bd7443583f583cf12b80bcd8a55c6457a66ef2b
Udnlx <Udnlx@yandex.ru>
2025-01-13 10:26:55 +0300

    небольшие правки по api

--- site/templates/api/tbot/GET/getprice.php
+++ site/templates/api/tbot/GET/getprice.php
@@ -30,6 +30,10 @@ if ($input->get['bus'] && $input->get['sstation'] && $input->get['fstation'] && 
 			$finish_title = '';
 		}
 
+		$received_price = 4000;
+		if ($age == 'child') {
+			$received_price = $received_price/2;
+		}
 		foreach ($bus_page->table_price as $item_price) {
 			if ($item_price->name_station == $start_title && $item_price->name_station_finish == $finish_title) {
 				$received_price = $item_price->price_ticket;

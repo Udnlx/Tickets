a8c2a0d0b18a89a6a7c57112e9833456c999324b
Udnlx <Udnlx@yandex.ru>
2025-04-08 11:49:34 +0300

    в апи по свободным местам добавили отрисовку массового резервирования

--- site/templates/api/tbot/GET/getseats.php
+++ site/templates/api/tbot/GET/getseats.php
@@ -8,10 +8,31 @@ if ($input->get['bus'] && $input->get['data']) {
 	$id_bus = $input->get('bus');
 	$data = $input->get('data');
 
+	$mass_reserv_seats_page = $pages->get('template=reserv_seats, id_bus=' . $id_bus . ', date_depart=' . $data . '');
+	if ($mass_reserv_seats_page->id > 0) {
+	    $arr_mass_reserv_seat_agent = explode(',', $mass_reserv_seats_page->mass_reserv_seats_agent);
+	    $arr_mass_reserv_seat = explode(',', $mass_reserv_seats_page->mass_reserv_seats);
+	} else {
+	    $arr_mass_reserv_seat_agent = [0];
+	    $arr_mass_reserv_seat = [0];
+	}
+	$mass_reserv_seat = [];
+	foreach ($arr_mass_reserv_seat as $arr_mass_reserv_seat_item) {
+	    $mass_reserv_seat[] = (int)$arr_mass_reserv_seat_item;
+	}
+	foreach ($arr_mass_reserv_seat_agent as $arr_mass_reserv_seat_agent_item) {
+	    $mass_reserv_seat[] = (int)$arr_mass_reserv_seat_agent_item;
+	}
+	$mass_reserv_seat = array_diff($mass_reserv_seat, [0]);
+	// echo '<pre>'; print_r($mass_reserv_seat); echo '</pre>';
+
 	$reserv_seat = $pages->find('template=purchased_tickets, id_bus=' . $id_bus . ', date_depart=' . $data . ',sort=seat');
 	$arr_reserv_seat = [];
 	foreach ($reserv_seat as $reserv_seat_item) {
 	    $arr_reserv_seat[] = (int)$reserv_seat_item->seat;
+	}
+	foreach ($mass_reserv_seat as $mass_reserv_seat_item) {
+	    $arr_reserv_seat[] = (int)$mass_reserv_seat_item;
 	}
 
 	$max_seat = 53;

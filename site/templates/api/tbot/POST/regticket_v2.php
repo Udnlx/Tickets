9052dc88206fc76b87b7b0976b054d9b665ad460
Udnlx <Udnlx@yandex.ru>
2025-08-27 12:30:25 +0300

    в апи добавили лог по ошибке confirmOrder

--- site/templates/api/tbot/POST/regticket_v2.php
+++ site/templates/api/tbot/POST/regticket_v2.php
@@ -383,6 +383,7 @@ if (isset($data['idBus'])) {
 				    }
 				    catch (SoapFault $soapFault){
 				        $sb_log .=  'Не удалось вызвать функцию confirmOrder;';
+				        $sb_log .=  'Значение orderId=' . $answer_book_order['id'] . ';';
 				        $info_json = json_encode($soapFault);
 				        $sb_log .=  $info_json;
 				    }
